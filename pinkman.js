b8a1994978d5bb8ec0c4ae3181585a88967e718a
Adelya <54155373+Delyaday@users.noreply.github.com>
2019-09-20 22:26:03 +0200

    Add files via upload

---
+++ pinkman.js
@@ -0,0 +1,22 @@
+(function ($) {
+    "use strict";
+
+    var guessWord = prompt ("Какое слово я задумала?");
+    if (guessWord == "сэндвич") {
+        alert ("Правильно!");
+    } else if (guessWord == "король") {
+        alert ("Угадал");
+    } else {
+        prompt ("Попробуй еще!");
+    }
+
+
+     
+    
+
+
+
+  
+    
+    
+})(jQuery);
